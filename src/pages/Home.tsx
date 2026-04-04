295665c97af41f35659495c8106d818d9ead5a20
Stubbsty <stubbs3256@gmail.com>
2026-04-05 07:55:42 +1000

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -63,7 +63,7 @@ export default function Home() {
         />
         <div className="absolute inset-0 bg-[#0a2744]/50" />
         
-        <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-10 pb-16">
+        <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-10 pb-24">
           <div className="max-w-3xl">
             <div className="flex items-center gap-3 mb-6">
               <span className="h-px w-10 bg-[#C9A84C]" />
